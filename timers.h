2e4640f99d231a8e882fa43f7b7b28565d443e02
Mike Lautman <mlautman@seas700.wireless-pennnet.upenn.edu>
2014-10-26 18:16:38 -0400

    timers functions commented

--- timers.h
+++ timers.h
@@ -8,12 +8,90 @@
  *********************************************
  */
 
-bool setup_loop_speed(int freq);
+////////////////////////////////////////////////
+// SET_LOOP_SPEED
+//
+// Functionality :
+//   - setup main timer (timer_3) to run at loop_frequency
+//
+// Parameters
+//   - frequency desired as an integer. (1000 -> 1khz)
+//
+// Returns
+//   - success or failure
+bool set_loop_speed(int freq);
 
-bool setup_timer();
+////////////////////////////////////////////////
+// LOOP_READY
+//
+// Functionality:
+//   - Checks the loop timer (timer_3) to see if
+//      1/1000 seconds have passed since last flag
+//
+// Parameters
+//
+// Returns
+//   - ready or not as boolean
+bool loop_ready(void);
 
-bool timer_clear();
 
-unsigned long timer_now();
+////////////////////////////////////////////////
+// CONFIG_STOPWATCH
+//
+// Functionality:
+//   - setup timer (timer_4) to time things
+//
+// Parameters
+//   - frequency desired as an integer. (1000 -> 1khz)
+//
+// Returns
+//   - success or failure
+bool config_stopWatch();
 
-unsigned long timer_delta(unsigned long start);
+
+////////////////////////////////////////////////
+// CLEAR_STOPWATCH
+//
+// Functionality:
+//   - clear timer (timer_4) counter to 0
+//
+// Parameters:
+//
+// Returns
+//   - success or failure
+bool clear_stopWatch();
+
+
+
+////////////////////////////////////////////////
+// STOPWATCH_NOW
+//
+// Functionality:
+//   - get the time since last stopWatch clear
+//
+// Parameters:
+//
+// Returns
+//   - unsigned long (NEEDS UNITS!!!)
+unsigned long stopWatch_now(void);
+
+
+////////////////////////////////////////////////
+// STOPWATCH_GETDELTA
+//
+// Functionality:
+//   - get difference between start and stopWatch_now
+//
+// Parameters:
+//   - unsigned long : start = start time
+//
+// Returns
+//   - time delta.
+//
+// NOTES: DO NOT USE. THIS IS HERE FOR FUTURE
+// unsigned long stopWatch_getDelta(unsigned long start);
+
+
+
+
+
