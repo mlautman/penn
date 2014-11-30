bfe74b1518e1d1d400a7bb0c0fbf9c63a44e4c97
Mike Lautman <mikeblautman@gmail.com>
2014-11-30 15:44:31 -0500

    adding header for interpolate/vectorize fn

--- ML/preprocess.py
+++ ML/preprocess.py
@@ -4,6 +4,9 @@ import numpy as np
 #Class imports
 from scipy import ndimage
 
+# function imports
+
+from numpy import interp
 
 """
 preprocess image functions
@@ -121,6 +124,162 @@ def scale(image, threshold=10):
 
     # zoomed in img
     return X[mg]
+
+
+def interpolate(Y, return_arr_length):
+    dummy = np.ones(return_arr_length)
+    interp(dummy, )
+
+def create_feature_vect(motion_data, interpolation_len):
+    """
+    Create a single vector of features
+    [
+        interpXaccel, (interpolation_len long)
+        interpYaccel, (interpolation_len long)
+        interpZaccel, (interpolation_len long)
+
+        interpXvel, (interpolation_len long)
+        interpYvel, (interpolation_len long)
+        interpZvel, (interpolation_len long)
+
+        interpXpos, (interpolation_len long)
+        interpYpos, (interpolation_len long)
+        interpZpos, (interpolation_len long)
+
+        interpXgyro, (interpolation_len long)
+        interpYgyro, (interpolation_len long)
+        interpZgyro, (interpolation_len long)
+
+        interpXang, (interpolation_len long)
+        interpYang, (interpolation_len long)
+        interpZang, (interpolation_len long)
+
+        mean_Xaccel,(1 long)
+        std_Xaccel, (1 long)
+        rms_Xaccel, (1 long)
+
+        mean_Yaccel,(1 long)
+        std_Yaccel, (1 long)
+        rms_Yaccel, (1 long)
+
+        mean_Zaccel,(1 long)
+        std_Zaccel, (1 long)
+        rms_Zaccel, (1 long)
+
+
+        mean_Xvel,(1 long)
+        std_Xvel, (1 long)
+        rms_Xvel, (1 long)
+
+        mean_Yvel,(1 long)
+        std_Yvel, (1 long)
+        rms_Yvel, (1 long)
+
+        mean_Zvel,(1 long)
+        std_Zvel, (1 long)
+        rms_Zvel, (1 long)
+
+
+        mean_Xpos,(1 long)
+        std_Xpos, (1 long)
+        rms_Xpos, (1 long)
+
+        mean_Ypos,(1 long)
+        std_Ypos, (1 long)
+        rms_Ypos, (1 long)
+
+        mean_Zpos,(1 long)
+        std_Zpos, (1 long)
+        rms_Zpos, (1 long)
+
+
+        mean_Xgyro,(1 long)
+        std_Xgyro, (1 long)
+        rms_Xgyro, (1 long)
+
+        mean_Ygyro,(1 long)
+        std_Ygyro, (1 long)
+        rms_Ygyro, (1 long)
+
+        mean_Zgyro,(1 long)
+        std_Zgyro, (1 long)
+        rms_Zgyro, (1 long)
+
+        mean_Xang,(1 long)
+        std_Xang, (1 long)
+        rms_Xang, (1 long)
+
+        mean_Yang,(1 long)
+        std_Yang, (1 long)
+        rms_Yang, (1 long)
+
+        mean_Zang,(1 long)
+        std_Zang, (1 long)
+        rms_Zang, (1 long)
+
+        record_time, (1 long)
+
+    ]
+
+        XYZ
+            accel
+            vel
+            pos
+            gyro
+            ang
+                mean
+                std
+                rms
+
+
+    input:
+        motion_data : Transformed Acc, vel, pos, rot rate and angle values
+            k x 15 array.
+
+        interpolation_len : An integer value representing target resolution in sensor readings.
+    output:
+        feature : (interpolation_len + 3)* 15 + 1
+
+    """
+
+
+    # grab the record time
+    record_time = motion_data.shape[0]
+
+    # dictionaries are used to hold the agg stat values
+    mean = {}
+    std = {}
+    rms = {}
+
+    # mean, std, rms
+    num_agg_stat_vals = 3
+    # |[X, Y, Z]| * |[acc, vel, pos, rot, ang]|
+    num_dimensions = 15
+
+    block_size = interpolation_len + num_agg_stat_vals
+
+    # preallocate output array
+    out = np.zeros(
+        num_dimensions * (
+            interpolation_len + num_agg_stat_vals
+        ) + 1
+    )
+
+    # first grab the agg stat values for each of the 15 motion values
+    for i in range(15):
+        start_block = i * block_size
+        end_block = start_block + block_size
+        # MEAN
+        mean[i] = np.mean(motion_data[:,i])
+        # STD
+        std[i] = np.std(motion_data[:,i])
+        # RMS
+        sq_dist = motion_data[:,i] ** 2
+        mean_sq_dist = np.mean(sq_dist)
+        rms[i] = np.sqrt(mean_sq_dist)
+
+        # resized
+
 
 
 
