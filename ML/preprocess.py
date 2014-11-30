d802e03980ba849faaaa0fe91f790b84c56bb920
Mike Lautman <mikeblautman@gmail.com>
2014-11-30 16:52:58 -0500

    preprocess fixes, cleaning up unused ML dir stuff

--- ML/preprocess.py
+++ ML/preprocess.py
@@ -8,124 +8,6 @@ from scipy import ndimage
 
 from numpy import interp
 
-"""
-preprocess image functions
-    - Smear convolves the image with a 2D gaussian blur,
-    -
-"""
-def smear(image, sigma):
-    """
-    performs a 2D gaussian convolution with the image
-        parameters:
-            image: n x d numpy matrix
-            sigma: sigma term in the smearing
-        returns
-            blurryIm : n x d numpy matrix
-    """
-    return ndimage.filters.gaussian_filter(image, sigma, mode='nearest')
-
-def recenter(image):
-    # center_of_mass
-    y_c, x_c = ndimage.measurements.center_of_mass(np.array(image))
-    x_c = int(round(x_c))
-    y_c = int(round(y_c))
-
-
-    # center of image
-    n, d = image.shape
-
-    # skew down (add to y-index's to recenter)
-    skew_down = n/2 - y_c
-
-    # skew right (add to x-index's to recenter)
-    skew_right = d/2 - x_c
-
-    return ndimage.interpolation.shift(
-        image,
-        [skew_down, skew_right],
-        cval=0.0
-    )
-
-
-def normalize(image):
-    return image/np.sqrt(np.power(image, 2).sum())
-
-# ASSUME SQUARE!!
-def scale(image, threshold=10):
-    X = np.round(image, decimals=threshold)
-    n, d = X.shape
-    Yz, Xz = np.nonzero(X)
-
-    left = Xz.min()
-    right = Xz.max()
-    top = Yz.min()
-    bottom = Yz.max()
-
-    h_span = right - left
-    v_span = bottom - top
-
-    proportionX = float(h_span)/float(d)
-    proportionY = float(v_span)/float(n)
-
-    goal = .80
-
-    if proportionX > proportionY:
-        span = h_span
-        extents = d
-        proportion = proportionX
-
-    else:
-        span = v_span
-        extents = n
-        proportion = proportionY
-
-    zoom = goal/proportion
-
-    full = np.round(span/goal)
-
-    h_begin = (d - full)/2.
-    h_end   = (d + full)/2.
-
-
-    v_begin = (n - full)/2.
-    v_end   = (n + full)/2.
-
-
-    i_locations = (
-        np.int64(
-            np.floor(
-                np.linspace(
-                    h_begin,
-                    h_end,
-                    d
-                )
-            )
-        )
-    )
-
-    j_locations = (
-        np.int64(
-            np.floor(
-                np.linspace(
-                    v_begin,
-                    v_end,
-                    n
-                )
-            )
-        )
-    )
-
-    # create scaled img grid mapping
-    mg = np.meshgrid(
-        i_locations,
-        j_locations,
-        indexing='ij',
-    )
-
-    # zoomed in img
-    return X[mg]
-
-
 def interpolate(Y, return_arr_length):
     dummy = np.ones(return_arr_length)
     interp(dummy, )
@@ -134,89 +16,25 @@ def create_feature_vect(motion_data, interpolation_len):
     """
     Create a single vector of features
     [
-        interpXaccel, (interpolation_len long)
-        interpYaccel, (interpolation_len long)
-        interpZaccel, (interpolation_len long)
+        interpXacc, mean_Xacc, std_Xacc, rms_Xacc, (interp_len+3 long)
+        interpYacc, mean_Yacc, std_Yacc, rms_Yacc, (interp_len+3 long)
+        interpZacc, mean_Zacc, std_Zacc, rms_Zacc, (interp_len+3 long)
 
-        interpXvel, (interpolation_len long)
-        interpYvel, (interpolation_len long)
-        interpZvel, (interpolation_len long)
+        interpXvel, mean_Xvel, std_Xvel, rms_Xvel, (interp_len+3 long)
+        interpYvel, mean_Yvel, std_Yvel, rms_Yvel, (interp_len+3 long)
+        interpZvel, mean_Zvel, std_Zvel, rms_Zvel, (interp_len+3 long)
 
-        interpXpos, (interpolation_len long)
-        interpYpos, (interpolation_len long)
-        interpZpos, (interpolation_len long)
+        interpXpos, mean_Xpos, std_Xpos, rms_Xpos, (interp_len+3 long)
+        interpYpos, mean_Ypos, std_Ypos, rms_Ypos, (interp_len+3 long)
+        interpZpos, mean_Zpos, std_Zpos, rms_Zpos, (interp_len+3 long)
 
-        interpXgyro, (interpolation_len long)
-        interpYgyro, (interpolation_len long)
-        interpZgyro, (interpolation_len long)
+        interpXgyro, mean_Xgyro, std_Xgyro, rms_Xgyro, (interp_len+3 long)
+        interpYgyro, mean_Ygyro, std_Ygyro, rms_Ygyro, (interp_len+3 long)
+        interpZgyro, mean_Zgyro, std_Zgyro, rms_Zgyro, (interp_len+3 long)
 
-        interpXang, (interpolation_len long)
-        interpYang, (interpolation_len long)
-        interpZang, (interpolation_len long)
-
-        mean_Xaccel,(1 long)
-        std_Xaccel, (1 long)
-        rms_Xaccel, (1 long)
-
-        mean_Yaccel,(1 long)
-        std_Yaccel, (1 long)
-        rms_Yaccel, (1 long)
-
-        mean_Zaccel,(1 long)
-        std_Zaccel, (1 long)
-        rms_Zaccel, (1 long)
-
-
-        mean_Xvel,(1 long)
-        std_Xvel, (1 long)
-        rms_Xvel, (1 long)
-
-        mean_Yvel,(1 long)
-        std_Yvel, (1 long)
-        rms_Yvel, (1 long)
-
-        mean_Zvel,(1 long)
-        std_Zvel, (1 long)
-        rms_Zvel, (1 long)
-
-
-        mean_Xpos,(1 long)
-        std_Xpos, (1 long)
-        rms_Xpos, (1 long)
-
-        mean_Ypos,(1 long)
-        std_Ypos, (1 long)
-        rms_Ypos, (1 long)
-
-        mean_Zpos,(1 long)
-        std_Zpos, (1 long)
-        rms_Zpos, (1 long)
-
-
-        mean_Xgyro,(1 long)
-        std_Xgyro, (1 long)
-        rms_Xgyro, (1 long)
-
-        mean_Ygyro,(1 long)
-        std_Ygyro, (1 long)
-        rms_Ygyro, (1 long)
-
-        mean_Zgyro,(1 long)
-        std_Zgyro, (1 long)
-        rms_Zgyro, (1 long)
-
-        mean_Xang,(1 long)
-        std_Xang, (1 long)
-        rms_Xang, (1 long)
-
-        mean_Yang,(1 long)
-        std_Yang, (1 long)
-        rms_Yang, (1 long)
-
-        mean_Zang,(1 long)
-        std_Zang, (1 long)
-        rms_Zang, (1 long)
-
+        interpXang, mean_Xang, std_Xang, rms_Xang, (interp_len+3 long)
+        interpYang, mean_Yang, std_Yang, rms_Yang, (interp_len+3 long)
+        interpZang, mean_Zang, std_Zang, rms_Zang, (interp_len+3 long)
         record_time, (1 long)
 
     ]
@@ -242,7 +60,6 @@ def create_feature_vect(motion_data, interpolation_len):
 
     """
 
-
     # grab the record time
     record_time = motion_data.shape[0]
 
@@ -254,7 +71,7 @@ def create_feature_vect(motion_data, interpolation_len):
     # mean, std, rms
     num_agg_stat_vals = 3
     # |[X, Y, Z]| * |[acc, vel, pos, rot, ang]|
-    num_dimensions = 15
+    num_dimensions = 5
 
     block_size = interpolation_len + num_agg_stat_vals
 
@@ -280,6 +97,7 @@ def create_feature_vect(motion_data, interpolation_len):
 
         # resized
 
+    out[-1] = record_time
 
 
 
