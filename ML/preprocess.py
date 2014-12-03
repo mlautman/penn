7181dfa4614f83f536f785f33984d7c8e089b046
Mike Lautman <mikeblautman@gmail.com>
2014-12-03 14:59:45 -0500

    adding plot helpers, preprocess fns and merge data script

--- ML/preprocess.py
+++ ML/preprocess.py
@@ -1,18 +1,32 @@
+from __future__ import division
+
 # Module imports
 import numpy as np
 
 #Class imports
-from scipy import ndimage
 
 # function imports
+from scipy.interpolate import InterpolatedUnivariateSpline
+# import matplotlib.pyplot as plt
 
-from numpy import interp
+NUM_AGG_STAT_VALS = 3
 
-def interpolate(Y, return_arr_length):
-    dummy = np.ones(return_arr_length)
-    interp(dummy, )
 
-def create_feature_vect(motion_data, interpolation_len):
+def interpolate(y, return_arr_length):
+    x = np.linspace(0, 1, len(y))
+    xi = np.linspace(0, 1, return_arr_length)
+    ius = InterpolatedUnivariateSpline(x, y)
+    yi = ius(xi)
+    return yi
+
+def normalize(x, mean, std):
+    return (x-mean)/std
+
+def feature_vect_size(motion_data, interpolation_len):
+    num_dimensions = motion_data.shape[1]
+    return num_dimensions * (interpolation_len + NUM_AGG_STAT_VALS ) + 1
+
+def create_feature_vect(motion_data, interpolation_len, just_data=True):
     """
     Create a single vector of features
     [
@@ -24,9 +38,17 @@ def create_feature_vect(motion_data, interpolation_len):
         interpYvel, mean_Yvel, std_Yvel, rms_Yvel, (interp_len+3 long)
         interpZvel, mean_Zvel, std_Zvel, rms_Zvel, (interp_len+3 long)
 
+                interpXvel_int (interp_len)
+                interpYvel_int (interp_len)
+                interpZvel_int (interp_len)
+
         interpXpos, mean_Xpos, std_Xpos, rms_Xpos, (interp_len+3 long)
         interpYpos, mean_Ypos, std_Ypos, rms_Ypos, (interp_len+3 long)
         interpZpos, mean_Zpos, std_Zpos, rms_Zpos, (interp_len+3 long)
+
+                interpXpos_int (interp_len)
+                interpYpos_int (interp_len)
+                interpZpos_int (interp_len)
 
         interpXgyro, mean_Xgyro, std_Xgyro, rms_Xgyro, (interp_len+3 long)
         interpYgyro, mean_Ygyro, std_Ygyro, rms_Ygyro, (interp_len+3 long)
@@ -39,16 +61,6 @@ def create_feature_vect(motion_data, interpolation_len):
 
     ]
 
-        XYZ
-            accel
-            vel
-            pos
-            gyro
-            ang
-                mean
-                std
-                rms
-
 
     input:
         motion_data : Transformed Acc, vel, pos, rot rate and angle values
@@ -60,44 +72,68 @@ def create_feature_vect(motion_data, interpolation_len):
 
     """
 
+    # mean, std, rms
+    # |[X, Y, Z]| * |[acc, vel, pos, rot, ang]|
+    num_dimensions = motion_data.shape[1]
+
+
     # grab the record time
     record_time = motion_data.shape[0]
 
     # dictionaries are used to hold the agg stat values
-    mean = {}
-    std = {}
-    rms = {}
+    mean = [None] * num_dimensions
+    std = [None] * num_dimensions
+    rms = [None] * num_dimensions
 
-    # mean, std, rms
-    num_agg_stat_vals = 3
-    # |[X, Y, Z]| * |[acc, vel, pos, rot, ang]|
-    num_dimensions = 5
 
-    block_size = interpolation_len + num_agg_stat_vals
+    block_size = interpolation_len + NUM_AGG_STAT_VALS
 
     # preallocate output array
     out = np.zeros(
         num_dimensions * (
-            interpolation_len + num_agg_stat_vals
+            interpolation_len + NUM_AGG_STAT_VALS
         ) + 1
     )
 
     # first grab the agg stat values for each of the 15 motion values
-    for i in range(15):
-        start_block = i * block_size
-        end_block = start_block + block_size
-        # MEAN
-        mean[i] = np.mean(motion_data[:,i])
-        # STD
-        std[i] = np.std(motion_data[:,i])
+    for i in range(num_dimensions):
+        start = i * block_size
+        # resize
+        interp_end = start + interpolation_len
+        resized = interpolate(
+            motion_data[:,i],
+            interpolation_len,
+        )
+        # plt.subplot(1,2,1)
+        # plt.plot(motion_data[:,i])
+
+        # plt.subplot(1,2,2)
+        # plt.plot(resized)
+
+
+        # MEAN - remove offset
+        mean[i] = np.mean(resized)
+        resized -= mean[i]
+
+        # STD - normalize
+        std[i] = np.std(resized)
+        resized /= std[i]
+
         # RMS
-        sq_dist = motion_data[:,i] ** 2
+        sq_dist = resized ** 2
         mean_sq_dist = np.mean(sq_dist)
         rms[i] = np.sqrt(mean_sq_dist)
 
-        # resized
+
+        out[start:interp_end] = resized
+
+        out[interp_end] = mean[i]
+        out[interp_end + 1] = std[i]
+        out[interp_end + 2] = rms[i]
 
     out[-1] = record_time
+
+    return out
 
 
 
