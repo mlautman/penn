7e150f9539bfa848eb80f57aaa7b1a2239ef8ccf
Francisco de Villalobos <franciscodevillalobos@gmail.com>
2014-12-05 16:31:55 -0500

    Changed git ignore to ignore all the captured data

--- matlab/penn_test2.m
+++ matlab/penn_test2.m
@@ -39,7 +39,8 @@ end
 %{a
 % Open serial interface to M2
 if ~exist('s','var') && ~rerun
-s = serial('/dev/ttyACM0');
+% s = serial('/dev/ttyACM0');
+s = serial('/dev/tty.usbmodem411');
 s.baudrate = 115200;%57600;
 s.terminator = 'LF';
 fopen(s);
@@ -379,9 +380,9 @@ while(1)
             [log(start_ind:(end-1),3),processed(start_ind:(end-1),:)],'-append','precision', 12)
         
         % Integrated image
-        fig_im = getframe(1);
-        im = fig_im.cdata;
-        imwrite(im,[directory,'/','im_',char_writing,datetime,'.png'])
+%         fig_im = getframe(1);
+%         im = fig_im.cdata;
+%         imwrite(im,[directory,'/','im_',char_writing,datetime,'.png'])
     end
     % ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     
